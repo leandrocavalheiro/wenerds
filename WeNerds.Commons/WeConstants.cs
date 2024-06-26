82004ddde07a22db2fbec29b7ebde35740b47d71
Leandro Luiz G. Cavalheiro <leandro.cavalheiro.dev@outlook.com>
2024-06-26 08:38:59 -0300

    - Change name variables Crypt to env variables paterns - Add salt at call methods

--- WeNerds.Commons/WeConstants.cs
+++ WeNerds.Commons/WeConstants.cs
@@ -8,5 +8,12 @@ public static class WeConstants
     public const string EV_NAME_JWT_ISSUER= "WN_JWT_ISSUER";
     public const string EV_NAME_JWT_AUDIANCE = "WN_JWT_AUDIANCE";
 
+    public const string EV_NAME_CRYPT_SALT = "WN_CRYPT_SALT";
+    public const string EV_NAME_CRYPT_KEY = "WN_CRYPT_SALT";
+    public const string EV_NAME_CRYPT_INTERATIONS = "WN_CRYPT_SALT";
 
+
+    public const string DEFAULT_CRYPT_SALT = "b7d72229-5d7b-4889-893d-635ebadd05cc";
+    public const string DEFAULT_CRYPT_KEY = "ce3b2eea-6677-4446-90cc-798c0443216d";
+    public const int DEFAULT_CRYPT_INTERATIONS = 1000;
 }
--- WeNerds.Services/Implementation/WeCriptography.cs
+++ WeNerds.Services/Implementation/WeCriptography.cs
@@ -3,17 +3,21 @@ using WeNerds.Services.Interfaces;
 using WeNerds.Commons.Extensions;
 using System.Security.Cryptography;
 using System.Text;
+using WeNerds.Commons;
 namespace WeNerds.Services.Implementation;
 
 public class WeCriptography(IConfiguration configuration) : IWeCriptography
-{
-    private readonly string _salt = configuration.GetSection("WeNerds:Criptography:Salt").Value.GetValueOrDefault("b7d72229-5d7b-4889-893d-635ebadd05cc");
-    private readonly string _cryptoKey = configuration.GetSection("WeNerds:Criptography:Key").Value.GetValueOrDefault("ce3b2eea-6677-4446-90cc-798c0443216d");
-    private readonly int _interations = configuration.GetSection("WeNerds:Criptography:Interations").Value.GetValueOrDefault("1000").ToInt();
+{    
+    private readonly string _salt = configuration[WeConstants.EV_NAME_CRYPT_SALT].GetValueOrDefault(WeConstants.DEFAULT_CRYPT_SALT);
+    private readonly string _cryptoKey = configuration[WeConstants.EV_NAME_CRYPT_KEY].GetValueOrDefault(WeConstants.DEFAULT_CRYPT_KEY);    
+    private readonly int _interations = configuration[WeConstants.EV_NAME_CRYPT_INTERATIONS].GetValueOrDefault(WeConstants.DEFAULT_CRYPT_INTERATIONS.ToString()).ToInt();
 
-    public string Encrypt(string text)
+    public string Encrypt(string text, string salt = null)
     {
-        var key = new Rfc2898DeriveBytes(_cryptoKey, Encoding.UTF8.GetBytes(_salt), _interations, HashAlgorithmName.SHA512).GetBytes(32);
+        if (string.IsNullOrWhiteSpace (salt))
+            salt = _salt;
+
+        var key = new Rfc2898DeriveBytes(_cryptoKey, Encoding.UTF8.GetBytes(salt), _interations, HashAlgorithmName.SHA512).GetBytes(32);
 
         using (var aesAlg = Aes.Create())
         {
@@ -33,9 +37,9 @@ public class WeCriptography(IConfiguration configuration) : IWeCriptography
             }
         }
     }
-    public bool CompareText(string text, string encryptedText)
+    public bool CompareText(string text, string encryptedText, string salt = null)
     {
-        text = Encrypt(text);
+        text = Encrypt(text, salt);
         return text == encryptedText;
     }
 }

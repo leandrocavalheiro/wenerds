e3b36a475c3832d4b5220b43000e6999190e5472
Leandro Luiz Guimarães Cavalheiro <ter_lgcavalheiro@maisaedu.com.br>
2024-06-27 18:32:29 -0300

    ExceptionHandler

---
+++ WeNerds.Middlewares/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.AspNetCore.Http;
+
+namespace WeNerds.Middlewares.Extensions;
+
+public static class MiddlewareExtensions
+{
+        public static bool IsApiRequest(this HttpContext context)
+            => context.Request.Path.StartsWithSegments("/api");
+}
